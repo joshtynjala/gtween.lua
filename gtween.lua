c720f7eff128c05efedfa39dcfb9362fe7055aaf
Josh Tynjala <joshtynjala@gmail.com>
2011-05-31 13:47:13 -0700

    added the rest of the easing functions from GTween.as. Updated readme to reflect addition of new easing functions.

--- gtween.lua
+++ gtween.lua
@@ -173,6 +173,106 @@ easing.inOutCubic = function(ratio)
 		return 4*ratio*ratio*ratio+1
 	end
 end
+local elasticA = 1;
+local elasticP = 0.3;
+local elasticS = elasticP/4;
+easing.inElastic = function(ratio)
+	if ratio == 0 or ratio == 1 then
+		return ratio
+	end
+	ratio = ratio - 1
+	return -(elasticA * math.pow(2, 10 * ratio) * math.sin((ratio - elasticS) * (2 * math.pi) / elasticP));
+end
+easing.outElastic = function(ratio)
+	if ratio == 0 or ratio == 1 then
+		return ratio
+	end
+	return elasticA * math.pow(2, -10 * ratio) *  math.sin((ratio - elasticS) * (2 * math.pi) / elasticP) + 1;
+end
+easing.inOutElastic = function(ratio)
+	if ratio == 0 or ratio == 1 then
+		return ratio
+	end
+	ratio = ratio*2-1
+	if ratio < 0 then
+		return -0.5 * (elasticA * math.pow(2, 10 * ratio) * math.sin((ratio - elasticS*1.5) * (2 * math.pi) /(elasticP*1.5)));
+	end
+	return 0.5 * elasticA * math.pow(2, -10 * ratio) * math.sin((ratio - elasticS*1.5) * (2 * math.pi) / (elasticP*1.5)) + 1;
+end
+easing.inExponential = function(ratio)
+	if ratio == 0 then
+		return 0
+	end
+	return math.pow(2, 10 * (ratio - 1))
+end
+easing.outExponential = function(ratio)
+	if ratio == 1 then
+		return 1
+	end
+	return 1-math.pow(2, -10 * ratio)
+end
+easing.inOutExponential = function(ratio)
+	if ratio == 0 or ratio == 1 then 
+		return ratio
+	end
+	ratio = ratio*2-1
+	if 0 > ratio then
+		return 0.5*math.pow(2, 10*ratio)
+	end
+	return 1-0.5*math.pow(2, -10*ratio)
+end
+easing.noneLinear = function(ratio)
+	return ratio
+end
+easing.inQuadratic = function(ratio)
+	return ratio*ratio
+end
+easing.outQuadratic = function(ratio)
+	return -ratio*(ratio-2)
+end
+easing.inOutQuadratic = function(ratio)
+	if ratio < 0.5 then
+		return 2*ratio*ratio
+	end
+	return -2*ratio*(ratio-2)-1
+end
+easing.inQuartic = function(ratio)
+	return ratio*ratio*ratio*ratio
+end
+easing.outQuartic = function(ratio)
+	ratio = ratio - 1
+	return 1-ratio*ratio*ratio*ratio
+end
+easing.inOutQuartic = function(ratio)
+	if ratio < 0.5 then
+		return 8*ratio*ratio*ratio*ratio
+	end
+	ratio = ratio - 1
+	return -8*ratio*ratio*ratio*ratio+1
+end
+easing.inQuintic = function(ratio)
+	return ratio*ratio*ratio*ratio*ratio
+end
+easing.outQuintic = function(ratio)
+	ratio = ratio - 1
+	return 1+ratio*ratio*ratio*ratio*ratio
+end
+easing.inOutQuintic = function(ratio)
+	if ratio < 0.5 then
+		return 16*ratio*ratio*ratio*ratio*ratio
+	end
+	ratio = ratio - 1
+	return 16*ratio*ratio*ratio*ratio*ratio+1
+end
+easing.inSine = function(ratio)
+	return 1-math.cos(ratio * (math.pi / 2))
+end
+easing.outSine = function(ratio)
+	return math.sin(ratio * (math.pi / 2))
+end
+easing.inOutSine = function(ratio)
+	return -0.5*(math.cos(ratio*math.pi)-1)
+end
 
 function new(target, duration, values, props)
 
